b66bd9349869d256ce8bf0437a013e70cf0c84ef
ChristopheLin <fengxup@gmail>
2026-07-22 01:08:18 +0800

    路徑新增練習(建立hello.blade.php新頁面、不建立新頁面只顯示字串練習)

--- routes/web.php
+++ routes/web.php
@@ -6,14 +6,30 @@ use Illuminate\Support\Facades\Route;
 //     return view('welcome');
 // });
 
-// Route::get('/', function () {
-//     return view('welcome123');
-// });
+Route::get('/', function () {
+    return view('welcome123');
+});
 
-// Route::get('/', function () {       //input
-//     return view('hello');           //output
-// });
+Route::get('/', function () {       //input
+    return view('laravel');           //output
+});
 
 Route::get('/feng', function () {
     return view('fenghome');
+});
+
+Route::get('/hi', function () {
+    return view('hello');
+});
+
+Route::get('/sum', function () {
+
+    $sum = 0;
+    for ($i = 1; $i <= 10; $i++) {
+        $sum = $sum + $i;
+    }
+    dd($sum);
+    
+    // print_r("Hello laravel sum");
+    // return view('hello');
 });
