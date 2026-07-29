9b6d611a9a06eb9bf0ae91a5d64ebaa6017522f4
ChristopheLin <fengxup@gmail.com>
2026-07-29 21:28:01 +0800

    練習將router與controller綁定

--- app/Http/Controllers/TestController.php
+++ app/Http/Controllers/TestController.php
@@ -6,5 +6,8 @@ use Illuminate\Http\Request;
 
 class TestController extends Controller
 {
-    //
+    public function index(){
+        dd('This is TEST page.');
+        // return 'Test page';
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,9 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\TestController;
+
+Route::get('/test', [TestController::class, 'index']);
 
 // Route::get('/', function () {
 //     return view('welcome');
