a9fe036f9e79f00d61fa1c8ecd6de511b1760d98
ChristopheLin <fengxup@gmail.com>
2026-08-22 20:37:44 +0800

    加入phones資料表的欄位

--- resources/sql/test.sql
+++ resources/sql/test.sql
@@ -5,6 +5,7 @@ CREATE TABLE `laravel_exercise`.`student` (
     `mobile` VARCHAR(20) NOT NULL DEFAULT '\"\"',
     PRIMARY KEY (`id`)
 ) ENGINE = InnoDB CHARSET = utf8mb4 COLLATE utf8mb4_unicode_ci;
+
 -- 
 
 -- 
@@ -12,6 +13,7 @@ ALTER TABLE
     `student` CHANGE `id` `id` INT(11) NOT NULL AUTO_INCREMENT COMMENT 'id',
     CHANGE `name` `name` VARCHAR(20) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL DEFAULT '\"\"' COMMENT '姓名',
     CHANGE `mobile` `mobile` VARCHAR(20) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL DEFAULT '\"\"' COMMENT '電話';
+
 -- 
 
 -- 
@@ -19,15 +21,14 @@ INSERT INTO
     `student` (`id`, `name`, `mobile`)
 VALUES
     (NULL, 'Apple', '0911'),
-    (NULL, 'Banana', '0922')
--- 
+    (NULL, 'Banana', '0922') -- 
+    --
+    CREATE TABLE `laravel_exercise`.`cats` (
+        `id` INT NOT NULL AUTO_INCREMENT,
+        `name` VARCHAR(20) NOT NULL DEFAULT '\"\"' COMMENT '姓名',
+        PRIMARY KEY (`id`)
+    ) ENGINE = InnoDB CHARSET = utf8mb4 COLLATE utf8mb4_unicode_ci;
 
---
-CREATE TABLE `laravel_exercise`.`cats` (
-    `id` INT NOT NULL AUTO_INCREMENT,
-    `name` VARCHAR(20) NOT NULL DEFAULT '\"\"' COMMENT '姓名',
-    PRIMARY KEY (`id`)
-) ENGINE = InnoDB CHARSET = utf8mb4 COLLATE utf8mb4_unicode_ci;
 --
 
 --
@@ -37,3 +38,18 @@ VALUES
     (NULL, 'Apple'),
     (NULL, 'Banana')
 --
+
+-- 
+-- INNER JOIN
+SELECT
+    `students`.`id`,
+    `students`.`name`,
+    `students`.`mobile`,
+    `phones`.`name`
+FROM
+    `students`
+INNER JOIN
+    `phones`
+ON
+    `students`.`id`=`phones`.`student_id`;
+-- 
